240a53f2526c56bc77243850df50b5fad7f86ba4
zdeblick <35307879+zdeblick@users.noreply.github.com>
2025-08-14 12:28:26 -0700

    Update main.py added health check

--- backend/main.py
+++ backend/main.py
@@ -518,6 +518,9 @@ async def get_user_ratings(author: str):
         
         return user_ratings
 
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
